88a68767d86e7c40f3db648129f0919756cb7fe5
muksh.developer@gmail.com <muksh.developer@gmail.com>
2019-06-12 18:57:27 -0700

    second commit

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-
+const cors = require('cors'); 
 // Serve static files....
 app.use(express.static(__dirname + '/dist/informationapp'));
 
